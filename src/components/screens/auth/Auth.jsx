28fbedcfa5330facdbb2441ad286fd370a16dd2e
navneet <navneet.v.maurya@gmail.com>
2024-03-18 23:16:24 +0530

    Done with Logos setup

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -1,6 +1,7 @@
 import { lazy } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "../../../styles/Auth.css";
+import Logo from "../../../assets/logo_transparent.png";
 
 const SigniIn = lazy(() => import("./Signin"));
 const Register = lazy(() => import("./Register"));
@@ -8,7 +9,8 @@ const Register = lazy(() => import("./Register"));
 const Auth = () => {
   return (
     <div className="auth">
-      <div>Logo</div>
+      <img className="logo" src={Logo} alt="" />
+
       <Routes>
         <Route path="signin" element={<SigniIn />} />
         <Route path="register" element={<Register />} />
